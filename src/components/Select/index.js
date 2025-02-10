5a107c0723f9d734814d52830fa9f1411233bff9
GryphoDev <wmauroux@icloud.com>
2025-02-10 12:02:15 +0100

    fix(select): collapse close when toutes filter is checked and add cursor pointer for better experience

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -18,7 +18,7 @@ const Select = ({
   const changeValue = (newValue) => {
     onChange(newValue);
     setValue(newValue);
-    setCollapsed(newValue);
+    setCollapsed(newValue || newValue === null);
   };
   return (
     <div className={`SelectContainer ${type}`} data-testid="select-testid">
